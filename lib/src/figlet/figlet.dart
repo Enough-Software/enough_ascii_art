164f537e111f333fcbe3c9ba079a7ddf7d6d1124
rupansh-arch <rupanshsekar@hotmail.com>
2020-10-17 19:23:58 +0530

    figlet: support Top to bottom rendering
    Kinda hacky ngl
    
    Signed-off-by: rupansh-arch <rupanshsekar@hotmail.com>

--- lib/src/figlet/figlet.dart
+++ lib/src/figlet/figlet.dart
@@ -1,16 +1,20 @@
 import 'dart:io';
 import 'dart:isolate';
 import 'package:enough_ascii_art/src/package_file_loader.dart';
-import 'package:resource/resource.dart' show Resource;
 import 'package:enough_ascii_art/src/figlet/parser.dart';
 import 'package:enough_ascii_art/src/figlet/renderer.dart';
 
 import 'font.dart';
 
+enum Direction {
+  LeftToRight,
+  TopToBottom
+}
+
 /// Helper class to render a FIGure
 class FIGlet {
   static Future<String> renderFIGureForFontName(
-      String text, String fontName) async {
+      String text, String fontName, Direction direction) async {
     fontName ??= 'cosmic';
     List<String> fontDefinition;
     String path;
@@ -25,11 +29,11 @@ class FIGlet {
     }
     var parser = Parser();
     var font = parser.parseFontDefinition(fontDefinition);
-    return renderFIGure(text, font);
+    return renderFIGure(text, font, direction);
   }
 
-  static String renderFIGure(String text, Font font) {
+  static String renderFIGure(String text, Font font, Direction direction) {
     var renderer = Renderer();
-    return renderer.render(text, font);
+    return renderer.render(text, font, direction);
   }
 }
